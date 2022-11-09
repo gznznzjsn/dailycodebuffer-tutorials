139bc1c223f6088db7967be315f081d82a9ce0d6
gznznzjsn <arik2046@gmail.com>
2022-11-10 00:13:33 +0300

    day One

---
+++ src/test/java/com/gznznzjsn/springdatajpatutorial/SpringDataJpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gznznzjsn.springdatajpatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
