ca6765fee8b00e9984369c7c9a4c7414a11063a6
gznznzjsn <arik2046@gmail.com>
2022-11-19 19:30:25 +0300

    project init

---
+++ src/main/java/com/gznznzjsn/dailycodebuffertutorials/DailycodebufferTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.gznznzjsn.dailycodebuffertutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailycodebufferTutorialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailycodebufferTutorialsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gznznzjsn/dailycodebuffertutorials/DailycodebufferTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gznznzjsn.dailycodebuffertutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailycodebufferTutorialsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
